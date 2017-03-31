930169d8c9f7dabde55a559b5ba3d70f2cd475ea
TuralSuleymani <suleymani.2ral@gmail.cm>
2017-03-31 19:27:22 +0400

    folder name changed

--- StrategyPattern/Providers/Specific/BakcellProvider.cs
+++ StrategyPattern/Providers/Specific/BakcellProvider.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace StrategyPatternViaRepository.Providers.Spesific
+namespace StrategyPatternViaRepository.Providers.Specific
 {
    internal class BakcellProvider : Provider
     {
